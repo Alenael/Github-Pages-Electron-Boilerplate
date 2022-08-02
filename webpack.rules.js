9c5c5e4e66355f414cf53fb703a823cec6fb105e
Alenael <Alenael>
2022-08-02 17:19:24 -0400

    More Fixes

--- webpack.rules.js
+++ webpack.rules.js
@@ -3,33 +3,6 @@ const isDevelopment = process.env.NODE_ENV !== "production";
 const isFastRefresh = process.env.FAST_REFRESH === "true";
 
 module.exports = [
-  // // Add support for native node modules
-  // {
-  //   // We're specifying native_modules in the test because the asset relocator loader generates a
-  //   // "fake" .node file which is really a cjs file.
-  //   test: /native_modules\/.+\.node$/,
-  //   use: "node-loader",
-  // },
-  // {
-  //   test: /\.(m?js|node)$/,
-  //   parser: { amd: false },
-  //   use: {
-  //     loader: "@vercel/webpack-asset-relocator-loader",
-  //     options: {
-  //       outputAssetBase: "native_modules",
-  //     },
-  //   },
-  // },
-  // {
-  //   test: /\.tsx?$/,
-  //   exclude: /(node_modules|\.webpack)/,
-  //   use: {
-  //     loader: "ts-loader",
-  //     options: {
-  //       transpileOnly: true,
-  //     },
-  //   },
-  // },
   {
     test: /\.(t|j)sx?$/,
     exclude: /node_modules/,
