67593cb7f72d62e1fbc0f5fc9d17d530e32092aa
Alenael <Alenael>
2022-08-02 19:05:42 -0400

    Yml

--- scripts/prepare-electron.js
+++ scripts/prepare-electron.js
@@ -1,20 +1,12 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const fs = require("fs");
 const path = require("path");
-const rimraf = require("rimraf");
-const copydir = require("copy-dir");
 const electronPkgJson = require("../electron/package.json");
 const parentPkgJson = require("../package.json");
 
-const src = path.join(__dirname, "..", ".webpack/renderer/main_window/");
-const dest = path.join(__dirname, "..", "electron", "out-resources");
-
-// Copy build dir to electron out-resources, which is where electron-forge
-// looks for source files.
-rimraf.sync(dest);
-copydir(src, dest, {});
-
-// Overwrite nested package.json version, because this is what electron-forge
-// uses to set the version in the Electron app.
+// Overwrite nested package.json info with parents - No longer needs to be updated twice
+electronPkgJson.name = parentPkgJson.name;
+electronPkgJson.productName = parentPkgJson.productName;
 electronPkgJson.version = parentPkgJson.version;
+electronPkgJson.description = parentPkgJson.description;
 fs.writeFileSync(path.join(__dirname, "..", "electron", "package.json"), JSON.stringify(electronPkgJson, null, 2));
