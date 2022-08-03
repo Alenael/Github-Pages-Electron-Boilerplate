39d54ae83a7fbe7ef79da0cc02d7cc8ea78aaeb9
Alenael <Alenael>
2022-08-02 21:55:12 -0400

    Test

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -6,7 +6,7 @@ function App() {
     <div className="App flex h-[100vh] flex-col items-center justify-center bg-[#eee]">
       <h1 className="text-4xl font-bold text-[#333]">Hi 👋, Tailwind test.</h1>
       <p className="font-italic m-[3px] text-sm">
-        Now you can use <code className="bg-[#ccc] p-[3px]">[]</code> to style your apps!
+        Now you can use <code className="bg-[#ccc] p-[3px]">[]</code> to style your apps
       </p>
     </div>
   );
