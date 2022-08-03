7384f2b9215e3b0d7c93fde5453b5a538c09b0c0
Alenael <Alenael>
2022-08-02 20:31:47 -0400

    Yml

--- scripts/prepare-electron.js
+++ scripts/prepare-electron.js
@@ -5,8 +5,8 @@ const electronPkgJson = require("../electron/package.json");
 const parentPkgJson = require("../package.json");
 
 // Overwrite nested package.json info with parents - No longer needs to be updated twice
-electronPkgJson.name = parentPkgJson.name;
-electronPkgJson.productName = parentPkgJson.productName;
+//electronPkgJson.name = parentPkgJson.name;
+//electronPkgJson.productName = parentPkgJson.productName;
 electronPkgJson.version = parentPkgJson.version;
 electronPkgJson.description = parentPkgJson.description;
 fs.writeFileSync(path.join(__dirname, "..", "electron", "package.json"), JSON.stringify(electronPkgJson, null, 2));
